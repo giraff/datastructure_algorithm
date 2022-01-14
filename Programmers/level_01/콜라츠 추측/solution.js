15094e620355f9aeabaeac5b142523987a9f6dac
giraff <hse05105@naver.com>
2022-01-14 22:54:19 +0900

    [Programmers/level01] 콜라츠 추측

---
+++ Programmers/level_01/콜라츠 추측/solution.js
@@ -0,0 +1,11 @@
+function solution(num) {
+  let answer = 0;
+  while (num !== 1) {
+    num = num % 2 === 0 ? num / 2 : num * 3 + 1;
+    answer++;
+    if (answer === 500) {
+      return -1;
+    }
+  }
+  return answer;
+}
