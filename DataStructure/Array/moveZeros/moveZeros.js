af9f833dbd805a7bb7105b3c341b7d7af0a4fa2b
HyeonJeong <40794857+giraff@users.noreply.github.com>
2022-03-24 12:03:27 +0900

    [Arrays] 2-2. moveZero 코드 구현

--- DataStructure/Array/moveZeros/moveZeros.js
+++ DataStructure/Array/moveZeros/moveZeros.js
@@ -3,9 +3,10 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function (nums) {
-  let onlyNums = 0;
-  let copyPosition = 0;
+  let onlyNums = 0; // 숫자만 찾아가는 인덱스
+  let copyPosition = 0; // 숫자를 카피할 자리를 가리키는 인덱스
 
+  // 숫자들을 순차적으로 배열 앞 쪽으로 복붙하기
   for (onlyNums = 0; onlyNums < nums.length; onlyNums++) {
     if (nums[onlyNums] !== 0) {
       nums[copyPosition] = nums[onlyNums];
@@ -13,6 +14,7 @@ var moveZeroes = function (nums) {
     }
   }
 
+  // 나머지 배열들을 0으로 채우기
   for (let idx = copyPosition; idx < nums.length; idx++) {
     nums[idx] = 0;
   }
